2de2b18b61440932cedbf4459a5b2c6f34964220
W. Bryan Smith <wbsmith@gmail.com>
2025-01-30 08:13:23 -0800

    adding back the filmstrip now that list is working.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import './styles/index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
