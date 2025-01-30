aa8146b07d1e48cc96ba2233a0ffb42ce1a1853b
W. Bryan Smith <wbsmith@gmail.com>
2025-01-30 07:25:14 -0800

    minimal version to list and show images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-// src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
-import './styles/index.css';
-import './config/amplify';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
