7398d59cd0f56a674861067ebe52f65d1fb4b83c
W. Bryan Smith <wbsmith@gmail.com>
2025-01-29 18:21:09 -0800

    update with identity pool ID config

--- src/config/amplify.js
+++ src/config/amplify.js
@@ -2,12 +2,16 @@
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-west-1:9e08878d-ca32-436e-b383-1af42a66a80d',
+    region: 'us-west-1',
+  },
   Storage: {
     AWSS3: {
       bucket: 'wbryansmith.org',
       region: 'us-west-1',
       customPrefix: {
-        public: ''  // Ensures no extra prefix in our paths
+        public: ''
       }
     }
   }
