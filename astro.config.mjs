82d2d38fe7c396967327c0d150250eec034e2567
W. Bryan Smith <wbsmith@gmail.com>
2026-05-02 02:08:39 -0700

    new vanity page build; Astro amplify correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import amplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: amplify(),
 });
